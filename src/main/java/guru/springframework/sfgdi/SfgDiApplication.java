6051d8854c00f3ec2ddebdf20b8752ec7f777e40
rodrigo-rufino <rodrigo.ribeiro@quintoandar.com.br>
2020-11-06 11:37:41 -0300

    primary beans

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -16,8 +16,9 @@ public class SfgDiApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+
+		System.out.println(" ------------ Primary Bean ------------ ");
+		System.out.println(myController.sayHello());
 
 		System.out.println(" ------------ Property Injected ------------ ");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
