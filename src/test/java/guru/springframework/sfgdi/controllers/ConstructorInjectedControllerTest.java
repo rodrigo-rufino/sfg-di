6999d86e347825024bdcae80d49fe14e9884d7e7
rodrigo-rufino <rodrigo.ribeiro@quintoandar.com.br>
2020-11-06 11:23:40 -0300

    using qualifiers

--- src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -1,10 +1,9 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.ConstructorInjectedGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 class ConstructorInjectedControllerTest {
 
     ConstructorInjectedController controller;
@@ -12,7 +11,7 @@ class ConstructorInjectedControllerTest {
     @BeforeEach
     void setUp() {
         // injecting GreetingServiceImpl into the controller
-        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+        controller = new ConstructorInjectedController(new ConstructorInjectedGreetingService());
     }
 
     @Test
